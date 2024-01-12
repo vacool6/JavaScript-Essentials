90297dceba3a4e9ec36881eefda3725388788b5a
Kittur Sameer <42382545+vacool6@users.noreply.github.com>
2024-01-12 16:54:20 +0530

    Create Currying.js

---
+++ Currying.js
@@ -0,0 +1,10 @@
+// Currying is a concept in functional programming where a function is transformed into a sequence of functions,
+// each taking a single argument. The result is a chain of functions, each taking one argument and returning another function, 
+// until the final function produces the desired result.
+
+// In JavaScript, currying can be implemented using closures and function chaining.
+
+
+
+
+
